62d72f5a5c8d8080e6433a774845439e63abe8a2
sinan <sinangoo@gmx.de>
2019-05-02 21:05:12 +0200

    commit message

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,19 +1,4 @@
-/*
-Bullet Continuous Collision Detection and Physics Library
-Copyright (c) 2003-2007 Erwin Coumans  http://continuousphysics.com/Bullet/
 
-This software is provided 'as-is', without any express or implied warranty.
-In no event will the authors be held liable for any damages arising from the use of this software.
-Permission is granted to anyone to use this software for any purpose, 
-including commercial applications, and to alter it and redistribute it freely, 
-subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
-*/
-
-///-----includes_start-----
 #include "btBulletDynamicsCommon.h"
 #include "BulletDynamics/ConstraintSolver/btGeneric6DofSpring2Constraint.h"
 
@@ -39,8 +24,6 @@ using namespace std;
 using json = nlohmann::json;
 const double PI = 3.1415926535897932384626433832795028841972;
 
-
-/// This is a Hello World program for running a basic Bullet physics simulation
 
 int main(int argc, char** argv)
 {
