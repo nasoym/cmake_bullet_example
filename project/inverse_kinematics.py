9d09f95a5e9a5262f1018c86f74b270f1e82cc19
Sinan Goo <sinan.goo@hypoport.de>
2019-06-03 13:10:27 +0200

    commit message

--- project/inverse_kinematics.py
+++ project/inverse_kinematics.py
@@ -1,4 +1,5 @@
 import math
+import argparse
 
 def lawOfCosines(a, b, c):
   return math.acos( (a*a + b*b - c*c) / (2 * a * b) )
@@ -34,9 +35,23 @@ def angles(x, y, len1, len2):
 #   | /. A1          D1
 #   (0,0)---------------------------------------x
 
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+# parser.add_argument('--sum', dest='accumulate', action='store_const',
+#                     const=sum, default=max,
+#                     help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+# print(args.integers)
+# print(args.integers[0])
+
 # print(angles(10,0,10,10))
 # print(angles(5,0,10,10))
-a,b = angles(3,5,4,5)
+# a,b = angles(5,0,4,5)
+a,b = angles(args.integers[0],args.integers[1],args.integers[2],args.integers[3])
+# print(a,b) 
 print(math.pi/2-a,math.pi-b) 
 # print(angles(4,8,4,5))
 
